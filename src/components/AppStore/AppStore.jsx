218d84d5cc1aafa32319a0cad0fd71ec1fccbadb
Rovshan13 <allakhverdievrovshann@gmail.com>
2024-01-14 16:17:30 +0400

    creat: AppStore section

--- src/components/AppStore/AppStore.jsx
+++ src/components/AppStore/AppStore.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import AppStoreImage from '../../assets/app_store.png'
+import PlayStoreImage from '../../assets/play_store.png'
+import Gif from '../../assets/mobile_bike.gif'
+
 
 const AppStore = () => {
   return (
-    <div>AppStore</div>
+    <section className='bg-gray-100 dark:bg-gray-800 py-10'>
+      <div className='container grid grid-cols-1 sm:grid-cols-2 place-items-center gap-6 '>
+
+        {/* Left section */}
+        <div data-aos='fade-up'>
+          <h1 className='text-3xl font-bold text-center text-gray-500'>Foodly is Available for Android and IOS</h1>
+
+          <div className='flex justify-center items-center'>
+            <img src={PlayStoreImage} alt="play store image" className='max-w-[150px] sm:max-w-[120px] md:max-w-[180px]' />
+            <img src={AppStoreImage} alt="App store image" className='max-w-[150px] sm:max-w-[120px] md:max-w-[180px]' />
+          </div>
+        </div>
+
+        {/* Right section */}
+        <div>
+          <img data-aos='zoom-in' src={Gif} alt="gif bike" className='w-[250px]' />
+        </div>
+
+      </div>
+    </section>
   )
 }
 
